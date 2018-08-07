0a77782a16f5474cdf637a1014ae345be6ec0624
zhengcheng <zhengcheng@followme-inc.com>
2018-08-07 16:44:29 +0800

    S

--- SQLTemplate/Tem.sql
+++ SQLTemplate/Tem.sql
@@ -1,4 +1,4 @@
-SET NAMES utf8mb4;  --建立连接的时候设置字符集
+SET NAMES utf8;  --建立连接的时候设置字符集
 
 -- ----------------------------
 -- 标准创建表 SQL模版
@@ -14,15 +14,22 @@ CREATE TABLE IF NOT EXISTS `t_broker_overnightrate_record`  (
   `Swaplong` varchar(64)   NOT NULL, -- 买单利息
   `Swapshort` varchar(64)   NOT NULL -- 卖单利息
   INDEX `idx_order_search`(`BrokerID`, `Account`, `OrderID`) USING BTREE --索引设置
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;  -- 后面的句子什么意思？
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;  
 
 
-----------------------------------------------------------------创表片断
-	--时间，不为空，默认值是现在的时间，自动更新本条时间
-      -- `UpdateTime`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  --时间可为空值
-    --`EndTime`        TIMESTAMP(3) NULL DEFAULT NULL
+/*
+COLLATE = utf8_general_ci   
+  ci是case insensitive的缩写，意思是大小写不敏感
+  utf8_general_ci本身是一种排序方式
+*/
 
 
----------------------------------
+/*
+更新时间的字段  不为空，默认值是现在的时间，自动更新本条时间
+  `UpdateTime`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  utf8_general_ci本身是一种排序方式
+
+时间可为空值
+  `EndTime`        TIMESTAMP(3) NULL DEFAULT NULL
+*/
 
--- mian.go
+++ mian.go
@@ -23,3 +23,17 @@ func recordTime() {
 	fmt.Println("App elapsed: ", elapsed)
 
 }
+
+// List去重 通过map主键唯一的特性过滤重复元素
+func RemoveRepByMap(slc []int) []int {
+	result := []int{}
+	tempMap := map[int]byte{} // 存放不重复主键
+	for _, e := range slc {
+		l := len(tempMap)
+		tempMap[e] = 0
+		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
+			result = append(result, e)
+		}
+	}
+	return result
+}
