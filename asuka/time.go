218e9ef9c9b59d078534b5b09079dd8bb51d4e88
zhengcheng <zhengcheng@followme-inc.com>
2018-10-26 15:57:17 +0800

    sd

--- asuka/time.go
+++ asuka/time.go
@@ -10,8 +10,9 @@ func Now() {
 	t1 = time.Now()
 }
 
-func Since(funcName string) {
+func Since(funcName string) time.Duration {
 	elapsed := time.Since(t1)
 	Printf("Elapsed Time:%s func name:%s", elapsed, funcName)
 	t1 = time.Now()
+	return elapsed
 }
