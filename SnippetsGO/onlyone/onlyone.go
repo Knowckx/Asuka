6f61a49171a02b2a3ab433ae4d6024083f03c20d
zhengcheng <zhengcheng@followme-inc.com>
2019-08-13 19:16:07 +0800

    123

--- SnippetsGO/onlyone/onlyone.go
+++ SnippetsGO/onlyone/onlyone.go
@@ -1,6 +1,7 @@
-package doone
+package OnlyOne
 
 import (
+	"fmt"
 	"sync"
 	"sync/atomic"
 )
@@ -10,9 +11,14 @@ type OnlyOne struct {
 	done uint32
 }
 
-func (o *OnlyOne) Do(f func()) {
+// 常见的函数签名类型
+type FuncType1 func() error
+type FuncType2 func() (interface{}, error)
+
+func (o *OnlyOne) Do(f FuncType1) error {
+	syncError := fmt.Errorf("We are working on the task! please try again later")
 	if atomic.LoadUint32(&o.done) == 1 {
-		return
+		return syncError
 	}
 	// Slow-path.
 	o.m.Lock()
@@ -21,6 +27,25 @@ func (o *OnlyOne) Do(f func()) {
 	if o.done == 0 {
 		atomic.StoreUint32(&o.done, 1)
 		defer atomic.StoreUint32(&o.done, 0)
-		f()
+		err := f()
+		return err
 	}
+	return syncError
+}
+
+func (o *OnlyOne) DoType2(f FuncType2) (interface{}, error) {
+	syncError := fmt.Errorf("We are working on the task! please try again later")
+	if atomic.LoadUint32(&o.done) == 1 {
+		return nil, syncError
+	}
+	o.m.Lock()
+	defer o.m.Unlock()
+
+	if o.done == 0 {
+		atomic.StoreUint32(&o.done, 1)
+		defer atomic.StoreUint32(&o.done, 0)
+		out, err := f()
+		return out, err
+	}
+	return nil, syncError
 }
