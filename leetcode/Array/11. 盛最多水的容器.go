57ae4c9eb83355d5a410168952d79da87d21ac56
unknown <knowckx@foxmail.com>
2020-05-21 17:48:05 +0800

    ti11

---
+++ leetcode/Array/11. 盛最多水的容器.go
@@ -0,0 +1,33 @@
+package Array
+
+// 数组的每个数字表示一个高，抽两个数字，与他们之间的距离，构成一个容器。求此容器的面积
+// 输入：[1,8,6,2,5,4,8,3,7]
+// 输出：49
+
+// 解法 双指针指向数组两端，两端的数哪个比较小，那就这指针向中间移动
+func maxArea(height []int) int {
+	le := 0
+	ri := len(height) - 1
+	res := height[1] - height[0]
+	for le < ri {
+		min := GetMin(height[ri], height[le])
+		area := min * (ri - le)
+		if area > res {
+			res = area
+		}
+
+		if height[le] < height[ri] {
+			le++
+		} else {
+			ri--
+		}
+	}
+	return res
+}
+
+func GetMin(v1 int, v2 int) int {
+	if v1 <= v2 {
+		return v1
+	}
+	return v2
+}
