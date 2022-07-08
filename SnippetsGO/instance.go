e18444d235b7fa437b977d6831ad5075a475f4dd
cheng.zheng01 <cheng.zheng01@sap.com>
2022-07-08 10:22:33 +0800

    log

---
+++ SnippetsGO/instance.go
@@ -0,0 +1,51 @@
+package SnippetsGO
+
+import (
+	"github.com/rs/zerolog/log"
+)
+
+/* 工作中经常有这样的需求，需要一个全局变量 或者 单例，用这个变量来记一些状态数据 */
+
+var fInstance *NotifyFilter // 用一个map来记录这个元素是不是已经出现过了
+
+// 入口
+func NotifyFilterEntrence(accessId string) error {
+	// each ID only use once
+	if FilterAccessId(accessId) {
+		log.Info().Msgf("filter ID. skip notify.", "id", accessId)
+		return nil
+	}
+	return nil
+}
+
+type NotifyFilter struct {
+	data map[string]string
+}
+
+// true: Repeated
+func (t *NotifyFilter) IsRepeated(in string) bool {
+	_, ok := t.data[in]
+	if ok {
+		return true
+	}
+	if len(t.data) > 100000 { // reset
+		log.Info().Msgf("NotifyFilter sive is too big. reset.")
+		t.data = make(map[string]string, 100000)
+	}
+	t.data[in] = ""
+	log.Info().Msgf("NotifyFilter add new unit: %s. Now Cap: %d", in, len(t.data))
+	return false
+}
+
+func NewNotifyFilter() *NotifyFilter {
+	out := NotifyFilter{}
+	out.data = make(map[string]string, 100000)
+	return &out
+}
+
+func FilterAccessId(accessId string) bool {
+	if fInstance == nil {
+		fInstance = NewNotifyFilter()
+	}
+	return fInstance.IsRepeated(accessId)
+}
