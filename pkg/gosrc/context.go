7dca5328232c56b8f85e4afa48b628d097f15f9e
knowckx <knowckx@foxmail.com>
2023-10-13 22:26:02 +0800

    11

--- pkg/gosrc/context.go
+++ pkg/gosrc/context.go
@@ -27,3 +27,26 @@ func Executor(ctx context.Context) {
 		}
 	}
 }
+
+// 模式2：通过chan来通知停止工作
+func TestChan() {
+	stopCh := make(chan int) // 结束信号
+	go Worker(stopCh)
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Stop")
+	stopCh <- 1
+}
+
+func Worker(stop chan int) {
+	for {
+		select {
+		case <-stop:
+			fmt.Println("Worker Stop!")
+			return
+		default:
+			fmt.Println("Working")
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
